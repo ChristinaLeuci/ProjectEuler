41d6998f6b22d54509d0ade5be2fa826f90308df
christinaleuci <christinaleuci@gmail.com>
2014-08-22 08:47:52 -0400

    completed Project 2

---
+++ Project2.rb
@@ -0,0 +1,22 @@
+require 'pry'
+class Project2
+  def initialize
+    puts fibonacci
+  end
+  def fibonacci
+    fiboarray = [1,2]
+    evensum = 0
+    while fiboarray[-2] < 4000000 && fiboarray[-1] < 4000000
+      fiboarray << (fiboarray[-1]+fiboarray[-2])
+    # create fibonacci array of all numbers until 4mil
+    # then go through that array and find all even
+    end
+    fiboarray.each do |number|
+      if number%2 == 0
+        evensum += number
+      end
+    end
+    return evensum
+  end
+  p2 = Project2.new
+end
